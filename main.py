a0eec9f3b8c159d6949b3eab08c180be44c935f7
lupingW <50098740+lupingW@users.noreply.github.com>
2023-04-14 19:00:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -181,9 +181,8 @@ def main(_user, _passwd, min_1, max_1):
     user = str(_user)
     password = str(_passwd)
     step = str(random.randint(min_1, max_1))
-    print(user)
-    print(password)
-    print('这是一个测试')
+    result2 = f"[{now}]\n账号：{user}\n修改步数（{step}\n"
+    print(result2)
     print("已设置为随机步数(" + str(min_1) + "~" + str(max_1) + ")")
     if user == '' or password == '':
         print("用户名或密码填写有误！")
