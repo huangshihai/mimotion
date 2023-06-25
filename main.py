2d18075369c643d78c7253fa4990462dec1bb6a5
StoneParkPlus <31660025+StoneParkPlus@users.noreply.github.com>
2023-06-26 02:35:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ def getBeijinTime():
         max_1 = 3500 * max_ratio
         # min_1 = int(K * min_1)
         # max_1 = int(K * max_1)
+           print("测试。")
          min_1 = 111
          max_1 = 111
     else:
